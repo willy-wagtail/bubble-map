0b9d1c440530bae39a33e181398d1b8ccd717444
William Cheung <william.cheung.c@gmail.com>
2023-01-06 23:25:59 +0000

    added City type

--- src/components/PointsOnMap.tsx
+++ src/components/PointsOnMap.tsx
@@ -3,10 +3,11 @@ import { geoNaturalEarth1, geoPath, geoGraticule, scaleSqrt, max } from "d3";
 
 import "./PointsOnMap.css";
 import { WorldAtlasData } from "../hooks/useWorldAtlas";
+import { City } from "../hooks/useWorldCities";
 
 export type PointsOnMapProps = {
   worldAtlas: WorldAtlasData;
-  cities: any;
+  cities: City[];
 };
 
 const projection = geoNaturalEarth1();
@@ -17,7 +18,8 @@ const graticulesPath = path(graticules());
 const globeOutlinePath = path({ type: "Sphere" });
 
 const pointSizeMaxRadius = 15;
-const pointSizeValueAccessor: any = (d: any) => d.population;
+const pointSizeMaxDomain = 50000000;
+const population = (d: City) => d.population;
 
 export default function PointsOnMap({
   worldAtlas: { land, interiors },
@@ -25,8 +27,10 @@ export default function PointsOnMap({
 }: PointsOnMapProps) {
   const interiorsPath = path(interiors);
 
+  const maxDomain = max(cities, population) || pointSizeMaxDomain;
+
   const pointSizeScale = scaleSqrt()
-    .domain([0, max(cities, pointSizeValueAccessor) as any])
+    .domain([0, maxDomain])
     .range([0, pointSizeMaxRadius]);
 
   return (
@@ -42,29 +46,25 @@ export default function PointsOnMap({
       {land.features.map((feature) => {
         const pathD = path(feature);
 
-        if (pathD === null) {
-          console.warn(
-            "The SVG path d attribute calculation returned null for a GeoJsonFeature"
-          );
-
-          return "";
-        }
-
-        return <path className="land" d={pathD} />;
+        return pathD !== null ? <path className="land" d={pathD} /> : null;
       })}
 
       {interiorsPath !== null ? (
         <path className="interiors" d={interiorsPath} />
       ) : null}
 
-      {cities.map((city: any) => {
-        const [x, y]: any = projection([city.lng, city.lat]);
+      {cities.map((city) => {
+        const xyCoordinates = projection([city.lng, city.lat]);
+
+        if (xyCoordinates === null) {
+          return null;
+        }
 
         return (
           <circle
-            cx={x}
-            cy={y}
-            r={pointSizeScale(pointSizeValueAccessor(city))}
+            cx={xyCoordinates[0]}
+            cy={xyCoordinates[1]}
+            r={pointSizeScale(population(city))}
           />
         );
       })}
